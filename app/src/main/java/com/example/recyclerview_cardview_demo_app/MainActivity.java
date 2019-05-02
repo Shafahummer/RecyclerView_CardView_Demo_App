3148509ef936152bf40fca07a5c9ec992d70c41c
Shafahummer <shafahummer11@gmail.com>
2019-05-02 21:53:38 +0530

    insert and delete cards

--- app/src/main/java/com/example/recyclerview_cardview_demo_app/MainActivity.java
+++ app/src/main/java/com/example/recyclerview_cardview_demo_app/MainActivity.java
@@ -4,10 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<ExampleItem> exampleList=new ArrayList<>();
+
+    Button buttonAdd,buttonDelete;
+    EditText editTextAdd,editTextDelete;
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
@@ -18,7 +27,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<ExampleItem> exampleList=new ArrayList<>();
+
+        generateFakeData();
+        recyclerViewConfig();
+
+        buttonAdd=findViewById(R.id.buttonadd);
+        buttonDelete=findViewById(R.id.buttondelete);
+        editTextAdd=findViewById(R.id.edittextadd);
+        editTextDelete=findViewById(R.id.edittextdelete);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s=editTextAdd.getText().toString();
+                if (TextUtils.isEmpty(s))
+                    editTextAdd.setError("Enter any value!!!");
+                else {
+                    int position = Integer.parseInt(editTextAdd.getText().toString());
+                    addCard(position);
+                }
+            }
+        });
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s=editTextDelete.getText().toString();
+                if (TextUtils.isEmpty(s))
+                    editTextDelete.setError("Enter any value!!!");
+                else {
+                    int position = Integer.parseInt(editTextDelete.getText().toString());
+                    deleteCard(position);
+                }
+
+            }
+        });
+
+        }
+
+
+    public void generateFakeData(){
 
         exampleList.add(new ExampleItem(R.drawable.act,"Clicked at Studio"));
         exampleList.add(new ExampleItem(R.drawable.oner,"Clicked at Italy"));
@@ -28,6 +75,9 @@ public class MainActivity extends AppCompatActivity {
         exampleList.add(new ExampleItem(R.drawable.fiver,"Clicked at India"));
         exampleList.add(new ExampleItem(R.drawable.sixr,"Clicked at US"));
 
+
+    }
+    public void recyclerViewConfig(){
 
         //Config for RecyclerView
         recyclerView=findViewById(R.id.recyclerView);
@@ -40,4 +90,13 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
     }
+    public void addCard(int position){
+        exampleList.add(position,new ExampleItem(R.drawable.act,"New Card Added!"));
+        adapter.notifyItemInserted(position);
+    }
+    public void deleteCard(int position){
+        exampleList.remove(position);
+        adapter.notifyItemRemoved(position);
+    }
 }
+
